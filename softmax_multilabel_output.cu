e9fc4a23b809a6c8b381160d6d80f509076a1ac6
jimxinbo <jimxinbo@gmail.com>
2016-02-18 20:58:00 +0800

    Create softmax_multilabel_output.cu

---
+++ softmax_multilabel_output.cu
@@ -0,0 +1,20 @@
+/*!
+ * Copyright (c) 2015 by Contributors
+ * \file softmax_multilabel_output.cu
+ * \brief
+ * modified from softmax_output.cu by Bo Xin
+*/
+
+#include "./bx_softmax_multilabel_output-inl.h"
+
+namespace mxnet {
+namespace op {
+template<>
+Operator *CreateOp<gpu>(SoftmaxMultilabelOutputParam param) {
+  return new SoftmaxMultilabelOutputOp<gpu>(param);
+}
+
+
+}  // namespace op
+}  // namespace mxnet
+
